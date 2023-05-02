0a090ca6ccbb40562cd3e5832ce86616f68d0f93
claire <yue.katou@gmail.com>
2023-05-02 10:51:55 -0700

    04) Building an API: Read One, Update, and Delete

--- app/routes/cat_routes.py
+++ app/routes/cat_routes.py
@@ -4,6 +4,7 @@ from app import db
 
 bp = Blueprint("cats", __name__, url_prefix="/cats")
 
+# GET ALL ENDPOINT
 @bp.route("", methods=["GET"])
 def handle_cats():
   cats = Cat.query.all()
@@ -13,6 +14,7 @@ def handle_cats():
   
   return jsonify(cats_list), 200
 
+# CREATE ENDPOINT
 @bp.route("", methods=["POST"])
 def create_cat():
   request_body=request.get_json()
@@ -26,4 +28,50 @@ def create_cat():
   db.session.add(new_cat)
   db.session.commit()
 
-  return make_response(f"Book {new_cat.name} successfully created", 201)
+  return make_response(f"Cat {new_cat.name} successfully created", 201)
+
+# GET ONE ENDPOINT
+@bp.route("/<id>", methods=["GET"])
+def handle_cat(id):
+    cat = validate_cat(id)
+
+    return jsonify(cat.make_cat_dict()), 200
+
+# UPDATE ONE ENDPOINT
+@bp.route("/<id>", methods=["PUT"])
+def update_cat(id):
+    cat = validate_cat(id)
+    request_body = request.get_json()
+
+    cat.color = request_body["color"]
+    cat.personality = request_body["personality"]
+    cat.name = request_body["name"]
+    
+    db.session.commit()
+
+    return make_response(f"Cat {cat.name} successfully updated", 200)
+
+
+# DELETE ONE ENDPOINT
+@bp.route("/<id>", methods=["DELETE"])
+def delete_cat(id):
+  cat = validate_cat(id)
+
+  db.session.delete(cat)
+  db.session.commit()
+
+  return make_response(f"Cat {cat.name} successfully deleted", 200)
+
+# HELPER FUNCTIONS
+def validate_cat(id):
+  try:
+      id = int(id)
+  except:
+    abort(make_response({"message": f"{id} was invalid"}, 400))
+
+  cat = Cat.query.get(id)
+  
+  if not cat:
+    abort(make_response({"message": f"Cat with id {id} was not found"}, 404))
+
+  return cat
