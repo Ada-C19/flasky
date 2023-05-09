0a4c05a102aae626d5f62c4af273ffcb5cac47cb
Mark Arciaga <mark.arciaga@adadevelopersacademy.org>
2023-05-09 10:19:44 -0700

    Update create_app to use Render db connection string env var.

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,9 @@ def create_app(test_config=None):
 
     if not test_config:
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-            "SQLALCHEMY_DATABASE_URI")
+            "RENDER_DATABASE_URI")
+        # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+        #     "SQLALCHEMY_DATABASE_URI")
     else:
         app.config['TESTING'] = True
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
